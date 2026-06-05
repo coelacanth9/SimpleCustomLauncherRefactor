c5710ea85022557ec05b1aff2c6d0830e7ec2d49
coelacanth9 <coelacanth9.dev@gmail.com>
2026-06-05 16:22:14 +0900

    build: versionCode を 19、versionName を 2.0 に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.coelacanth9.simplecustomlauncher"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 19
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
